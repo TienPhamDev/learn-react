fd7dfc4f98a4205faf538c4ad9d9548e3283c158
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-13 16:51:41 -0400

    cm

--- dev-card/src/App.jsx
+++ dev-card/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='container mx-auto w-1/2'>
+    <div className='container mx-auto w-1/2 mt-8'>
       <Card />
 
     </div>
@@ -13,11 +13,32 @@ function App() {
 };
 function Card(){
   return (
-    <div className='border-2 border-slate-900'>
+    <div className='border-4 border-slate-900'>
       <img src="" alt="" />
-      <h1 className='text-xl'>Tien Pham</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eaque expedita iusto sit blanditiis beatae reiciendis illum. Repudiandae praesentium quae ducimus, vel sed, ad dolorem tenetur, vero numquam fugit sapiente?</p>
+      <Bio/>
     </div>
   );
 }
+function Bio(){
+  return (
+    <div className='p-8'>
+      <h1 className='text-xl'>Tien Pham</h1>
+      <p className='py-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eaque expedita iusto sit blanditiis beatae reiciendis illum. Repudiandae praesentium quae ducimus, vel sed, ad dolorem tenetur, vero numquam fugit sapiente?</p>
+      <Skill 
+        name = "HTML"
+        color = "bg-red-500"
+        emoji = "&#129299;"
+      />
+    </div>
+  );
+}
+// &#128077;
+function Skill(props){
+  let style = `p-2 rounded-md ${props.color}`;
+  return (
+    <span className={style}>
+      {props.name} {props.emoji}
+    </span>
+  );
+};
 export default App
