a294dc640e163f4a0a8b0ef2af021edd79f3c7d8
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-13 22:12:25 -0400

    cm

--- dev-card/src/App.jsx
+++ dev-card/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='container mx-auto w-1/2 mt-8'>
+    <div className='container mx-auto w-1/2'>
       <Card />
 
     </div>
@@ -13,7 +13,7 @@ function App() {
 };
 function Card(){
   return (
-    <div className='border-4 border-slate-900'>
+    <div className='border-4 border-slate-900 mt-8'>
       <img src="" alt="" />
       <Bio/>
     </div>
@@ -24,15 +24,32 @@ function Bio(){
     <div className='p-8'>
       <h1 className='text-xl'>Tien Pham</h1>
       <p className='py-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eaque expedita iusto sit blanditiis beatae reiciendis illum. Repudiandae praesentium quae ducimus, vel sed, ad dolorem tenetur, vero numquam fugit sapiente?</p>
-      <Skill 
-        name = "HTML"
-        color = "bg-red-500"
-        emoji = "&#129299;"
-      />
+      <div className='flex gap-2'>
+        <Skill 
+          name = "HTML"
+          color = "bg-red-500"
+          emoji = "&#129299;"
+          />
+        <Skill
+          name = "JAVASCRIPT"
+          color = "bg-yellow-500"
+          emoji = "&#128077;"
+          />
+        <Skill
+          name = "REACT"
+          color = "bg-sky-500"
+          emoji = "&#129299;"
+        />
+        <Skill
+          name = "GITHUB"
+          color = "bg-slate-500"
+          emoji = "&#129299;"
+        />
+      </div>
     </div>
   );
 }
-// &#128077;
+//thumb up &#128077; nerd face &#129299;
 function Skill(props){
   let style = `p-2 rounded-md ${props.color}`;
   return (
