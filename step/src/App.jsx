0252179f072b613cb0e75dc124a25ff1fce00d9d
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-17 15:37:48 -0400

    cm

--- step/src/App.jsx
+++ step/src/App.jsx
@@ -2,14 +2,14 @@ function App(){
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
       <p className="message">Message</p>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button style={{backgroundColor:"#7950f2",color:"#fff"}}>Previous</button>
+        <button style={{backgroundColor:"#7950f2",color:"#fff"}}>Next</button>
       </div>
     </div>);
 }
