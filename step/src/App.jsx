8f09950d49ed2df5f1b94726a4b9c248452cf843
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-17 15:31:52 -0400

    cm

--- step/src/App.jsx
+++ step/src/App.jsx
@@ -1,5 +1,17 @@
 function App(){
-  return <div>Hello react</div>
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Message</p>
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>);
 }
 
 export default App;
