958f9393d97d64e2bd6a538df27c2cb710a64b7a
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-17 00:59:01 +0000

    cm

--- javascript-react/src/main.jsx
+++ javascript-react/src/main.jsx
@@ -104,12 +104,12 @@ function Footer(){
 }
 function Pizza({ pizzaObj }) {
     console.log(pizzaObj)
-    return (<li className="pizza">
+  return (<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
                 <img src={pizzaObj.photoName} alt={pizzaObj.name} />
                 <div>
                     <h3>{pizzaObj.name}</h3>
                     <p>{pizzaObj.ingredients}</p>
-                    <span>{pizzaObj.price + 3}</span>
+                    <span>{pizzaObj.soldOut ? "Sold out" : pizzaObj.price}</span>
                 </div>
             </li>);
 };
