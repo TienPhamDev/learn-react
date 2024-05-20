6c99a18848c8070cd8a1faf46670b8338c5991b1
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-20 05:03:37 +0000

    cm

--- date-counter/src/App.jsx
+++ date-counter/src/App.jsx
@@ -3,14 +3,6 @@ import { useState } from "react";
 function App() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-  function daysBetween(count) {
-    const oneDay = 24 * 60 * 60 * 1000; // Hours * minutes * seconds * milliseconds
-    const currentDate = new Date();
-
-    // const diffDays = Math.round(Math.abs((currentDate + count) / oneDay));
-    // const resultDate = new Date(diffDays).toLocaleString("en-US",{dateStyle:"long"})
-    return currentDate;
-  }
   function handlePlusStep() {
     setStep((s)=> s+1)
   }
@@ -31,14 +23,9 @@ function App() {
     <div>
       <button onClick={handleMinusCount}>-</button><span>Count : {count}</span><button onClick={handlePlusCount}>+</button>
     </div>
-    <p>{
-      count === 0 ?
-        `today is ${new Date().toLocaleString("en-US", { dateStyle: "long" })}`
-        : 
-        `${count} days from today is ${daysBetween(count)}`
-      }
-    </p>
+    <p></p>
   </div>
 }
+
 export default App;
 // count} today is {new Date().toLocaleString("en-US",{dateStyle:"long"})
