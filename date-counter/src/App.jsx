46a2b46e16d941dcb81bbc6e650159140f88026e
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-05-20 23:13:52 -0400

    cm

--- date-counter/src/App.jsx
+++ date-counter/src/App.jsx
@@ -16,6 +16,8 @@ function App() {
   function handleMinusCount() {
     setCount(s => s - step)
   }
+  const date = new Date();
+  date.setDate(date.getDate() + count)
   return <div style={{ textAlign: "center" }}>
     <div>
       <button onClick={handleMinusStep}>-</button><span>step : {step}</span><button onClick={handlePlusStep}>+</button>
@@ -23,7 +25,17 @@ function App() {
     <div>
       <button onClick={handleMinusCount}>-</button><span>Count : {count}</span><button onClick={handlePlusCount}>+</button>
     </div>
-    <p></p>
+    <p>
+      <span>
+        {count === 0 
+        ? `Today is ` 
+        : count > 0 
+        ? `${count} days from today is `
+        : `${Math.abs(count)} days ago was ` 
+      }
+      </span>
+      <span>{date.toLocaleString()}</span>
+    </p>
   </div>
 }
 
