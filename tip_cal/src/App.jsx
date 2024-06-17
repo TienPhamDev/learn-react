c14fa4b26e700fa6f8a8ca1380665e5e9481b77f
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-06-17 18:33:55 +0700

    cm

--- tip_cal/src/App.jsx
+++ tip_cal/src/App.jsx
@@ -6,17 +6,19 @@ function App() {
   function handleInput(e){
     setBill(Number(e.target.value))
   }
+  const [percentage1,setPercentage1] = useState(0)
+  const [percentage2,setPercentage2] = useState(0)
   return (
     <>
       <Bill bill={bill} onChangeBill = {handleInput}/>
-      <ServiceTips>
+      <ServiceTips value = {percentage1} onChangeValue={setPercentage1}>
         How did you like the services?  
       </ServiceTips>
-      <ServiceTips>
+      <ServiceTips value={percentage2} onChangeValue={setPercentage2}>
         How did your friend like the service? 
       </ServiceTips>
-      <OutPut />
-      <Reset />
+      <OutPut bill = {bill} percentage1 = {percentage1} percentage2 ={percentage2}/>
+      <Reset onChangeBill={setBill} onChangePercent1 = {setPercentage1} onChangePercent2 ={setPercentage2}/>
     </>
   )
 }
@@ -27,15 +29,12 @@ function Bill({bill , onChangeBill}){
     <input type="text" value={bill} onChange={e => onChangeBill(e)} />
   </div>
 }
-function ServiceTips({children}){
-  const [value,setValue] = useState(0)
-  function handleSelect(e){
-    setValue(Number(e.target.value))
-  }
+function ServiceTips({value,onChangeValue,children}){
+  
   return (
     <div>
       <label>{children}</label>
-      <select value={value} onChange={(e) => handleSelect(e)}>
+      <select value={value} onChange={(e) => onChangeValue(Number(e.target.value))}>
         <option value={0}>Dislike(0)</option>
         <option value={5}>Okay(5%)</option>
         <option value={10}>Like(10%)</option>
@@ -44,12 +43,18 @@ function ServiceTips({children}){
     </div>
   )
 }
-function OutPut(){
+function OutPut({bill,percentage1,percentage2}){
+  const total = bill + (bill * (percentage1 - percentage2)/100)
   return <h3>
-    You pay X ($a + $b tip)
+    You pay {total} ({bill} + {bill * (percentage1 - percentage2)/100} tip)
   </h3>
 }
-function Reset(){
-  return <button>Reset</button>
+function Reset({onChangeBill,onChangePercent1,onChangePercent2}){
+  function handleClick(){
+    onChangeBill(0)
+    onChangePercent1(0)
+    onChangePercent2(0)
+  }
+  return <button onClick={handleClick}>Reset</button>
 }
 export default App
