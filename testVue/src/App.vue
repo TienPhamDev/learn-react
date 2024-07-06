7d52613c64711a0f52284ba648da7f212d57462d
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-06 18:20:31 +0700

    cm

--- testVue/src/App.vue
+++ testVue/src/App.vue
@@ -1,17 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name : "App",
+  data(){
+    return {
+      title : "My First Vue App"
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{title}}</h1>
 </template>
 
 <style scoped>
