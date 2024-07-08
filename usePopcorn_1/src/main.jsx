552fe63e24feec969c09b2a8fb0ef47237eac8c7
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-08 22:06:08 +0700

    cm

--- usePopcorn_1/src/main.jsx
+++ usePopcorn_1/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
+import App from './App.jsx'
+import './index.css'
 import StarRating from './StarRating'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StarRating maxRating={5} messages = {["Terrible","Bad","Okay","Good","Amazing"]}/>
-    <StarRating maxRating={5} color='red' size={24} defaultRating={3}/>
+    {/* <StarRating maxRating={5} messages = {["Terrible","Bad","Okay","Good","Amazing"]}/>
+    <StarRating maxRating={5} color='red' size={24} defaultRating={3}/> */}
+    <App />
   </React.StrictMode>
 )
