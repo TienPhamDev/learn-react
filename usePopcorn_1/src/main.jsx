c5fe0aa29b49604b7a092b6681a79755c9783cd0
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-08 15:21:15 +0700

    cm

---
+++ usePopcorn_1/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from './StarRating'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StarRating maxRating={5} messages = {["Terrible","Bad","Okay","Good","Amazing"]}/>
+    <StarRating maxRating={5} color='red' size={24} defaultRating={3}/>
+  </React.StrictMode>
+)
