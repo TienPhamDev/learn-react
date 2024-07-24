2cefb3e32baee6bc34254f1581f1d67103eed4cf
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-24 00:35:23 -0400

    end day

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,12 @@
+import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects"
+import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
+      {/* <MultipleEffects /> */}
+      <FetchData/>
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users,setUsers] = useState([])
+
+  useEffect(() => {
+    async function usersData () {
+      const res = await fetch(url)
+      const data = await res.json();
+      setUsers(data)
+      console.log(data)
+    }
+    usersData()
+  },[])
+  return <>
+    <h2>fetch data example</h2>
+    <main style={{margin:"24px auto",width:"40%"}}>
+      <ul >
+        {
+          users.map((user) => {
+            return <li key={user.id} style={{display:"flex",gap:"28px",margin:"16px 0"}}>
+              <img src={user.avatar_url} alt="avatar" style={{width:"75px",height:"75px"}}/>
+              <div style={{display:"flex",flexDirection:"column",alignItems:"start"}}>
+                <h3 style={{margin:"0px"}}>{user.login}</h3>
+                <a href={user.url} >Profile</a>  
+              </div>
+            </li>
+          })
+        }
+      </ul>
+    </main>
+  </> 
+
 };
 export default FetchData;
