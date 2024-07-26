e2167f07a09c96d1ad22e98db99524048602ce39
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-25 20:02:31 -0400

    cm

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,13 +1,12 @@
-import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects"
-import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
-import MultipleReturnsFetchData from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+
+import ToggleChallenge from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge"
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
       {/* <MultipleEffects /> */}
       {/* <FetchData/> */}
-      <MultipleReturnsFetchData />
+      <ToggleChallenge/>
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [state,setState] = useState(false)
+  return <h2>toggle challenge</h2>
 };
 
 export default ToggleChallenge;
