c140552f6288af23ef858a1705d0b70f7aa6993f
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-10-27 15:44:51 +0700

    cm

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,12 +1,13 @@
 
-import UserChallenge from "./tutorial/03-conditional-rendering/starter/07-user-challenge"
+import CleanupFunction from "./tutorial/02-useEffect/starter/05-cleanup-function"
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
       {/* <MultipleEffects /> */}
       {/* <FetchData/> */}
-      <UserChallenge/>
+      <CleanupFunction/>
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,20 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useEffect, useState } from "react";
 
+const CleanupFunction = () => {
+  const [toggle,setToggle] = useState(false)
+  return <div>
+    <button className="btn" onClick={()=>{setToggle(!toggle)}}>toggle Component</button>
+    {toggle && <SecondComponent/>}
+  </div>;
+};
+const SecondComponent= () => {
+  useEffect(()=>{
+    const h2Effect = document.querySelector("#secondCom")
+    h2Effect.style.backgroundColor = "red"
+    console.log("This is interesting");
+  },[])
+  return <div>
+    <h2 id="secondCom" >Second Component</h2>
+  </div>
+}
 export default CleanupFunction;
