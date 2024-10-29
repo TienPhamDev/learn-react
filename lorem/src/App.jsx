7bc1b2a57cdaedeeef63509b97c67f43d976475c
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-10-29 16:06:28 +0700

    cm

--- lorem/src/App.jsx
+++ lorem/src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import text from "./data";
+import { nanoid } from 'nanoid';
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count,setValue] = useState(1);
+  const [textArray,setTextArray] = useState([]);
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const numText = text.slice(0,count)
+    setTextArray(numText)
+    
+    console.log(numText);
+    
+  }
+  const handleInput = (e)=>{
+    setValue(Number(e.target.value))
+  }
+  // const id = nanoid();
+  return <div>
+      <form className="lorem-Form">
+      <input type="number" value={count} id="amount" step="1" min="1" max="8" onChange={(e)=> handleInput(e)} />
+      <button type="submit" onClick={(e) => handleSubmit(e)}>submit</button>
+    </form>
+    
+      {textArray.map((texts)=>{
+        return <div style={{marginBottom:"10px"}} key={nanoid()}><p>{texts}</p></div>
+      })}
+    </div>
 };
 export default App;
